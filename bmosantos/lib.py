46185d748ae8b29a18515f5940c5494d7350cf53
tobias ferreira <soutobias@yahoo.com.br>
2022-02-07 10:03:41 -0300

    bmocamp

--- bmosantos/lib.py
+++ bmosantos/lib.py
@@ -12,10 +12,9 @@ import plotly.graph_objects as go
 
 def get_data(token):
 
-    start_time = (datetime.utcnow() - timedelta(days=10)).strftime('%Y-%m-%d')
+    start_time = (datetime.utcnow() - timedelta(days=4)).strftime('%Y-%m-%d')
     time_now = (datetime.utcnow() + timedelta(days=1)).strftime('%Y-%m-%d')
     url=f'http://remobsapi.herokuapp.com/api/v1/data_buoys?buoy=22&start_date={start_time}&end_date={time_now}&token={token}'
-
     response = requests.get(url).json()
     df = pd.DataFrame(response)
     for i in df.columns:
@@ -60,7 +59,7 @@ def calculate_distance(df):
 
 def plot_map(df):
 
-    deployment_loc = [-25.508, -42.736]
+    deployment_loc = [-22.8637, -40.1527]
 
     m = folium.Map(location=deployment_loc, zoom_start=13)
 
